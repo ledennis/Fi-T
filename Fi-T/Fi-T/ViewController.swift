2bcef6ef07dd5a64740fda142532e9635518bd44
Dennis Le <le.dennis605@gmail.com>
2018-04-01 18:37:28 -0500

    Add initial project.

---
+++ Fi-T/Fi-T/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fi-T
+//
+//  Created by Dennis Le on 4/1/18.
+//  Copyright © 2018 Dennis Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
