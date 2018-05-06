5ab2b608c8bb74b24fd252bb0813e9b17e21ec7a
Dennis Le <le.dennis605@gmail.com>
2018-05-06 16:21:18 -0500

    Use UIViewPicker for sets.

--- Fi-T/Fi-T/AddWorkoutViewController.swift
+++ Fi-T/Fi-T/AddWorkoutViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 import os.log
 
-class AddWorkoutViewController: UIViewController, UITextFieldDelegate {
+class AddWorkoutViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var addWorkoutTF: UITextField!
-    @IBOutlet weak var addSetsTF: UITextField!
+    @IBOutlet var addSetsPV: UIView!
     @IBOutlet weak var addWeightsTF: UITextField!
     @IBOutlet weak var saveButton: UIBarButtonItem!
-
+    @IBOutlet weak var setsPV: UIPickerView!
+    
     var workout: Workout?
+    let setGroup = ["2 x 6-8", "3 x 5", "3 x 10", "4 x 8", "5 x 5"]
+    var selectedSet = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +31,7 @@ class AddWorkoutViewController: UIViewController, UITextFieldDelegate {
         if let workout = workout {
             navigationItem.title = workout.name
             addWorkoutTF.text   = workout.name
-            addSetsTF.text = workout.sets
+            setsPV.selectRow(selectPickerViewRow(string: workout.sets), inComponent: 0, animated:true)
             addWeightsTF.text = workout.weight
         }
         
@@ -66,7 +69,7 @@ class AddWorkoutViewController: UIViewController, UITextFieldDelegate {
         }
         
         let name = addWorkoutTF.text ?? ""
-        let sets = addSetsTF.text ?? ""
+        let sets = selectedSet
         let weight = addWeightsTF.text ?? ""
         
         workout = Workout(name: name, sets: sets, weight: weight)
@@ -85,11 +88,40 @@ class AddWorkoutViewController: UIViewController, UITextFieldDelegate {
     
     }
     
+    //MARK: UIPickerViewDelegate
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return setGroup[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return setGroup.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedSet = setGroup[row]
+    }
+    
     //MARK: Private Methods
     
     private func updateSaveButtonState() {
         // Disable the Save button if the text field is empty.
         let text = addWorkoutTF.text ?? ""
         saveButton.isEnabled = !text.isEmpty
+    }
+    
+    private func selectPickerViewRow(string: String) -> Int {
+        for (index, set) in setGroup.enumerated() {
+            if (string == set) {
+                return index
+            }
+        }
+        
+        fatalError("Could not find set to load for default set in AddWorkoutViewController.")
+        return -1
     }
 }
